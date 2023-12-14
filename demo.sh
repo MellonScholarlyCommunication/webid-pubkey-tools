b02b269800a2bbc48975af62035d25126c885ca8
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-12-14 09:36:34 +0100

    Adding a demo

---
+++ demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KEY="keys/rsa_priv.pem"
+URL="https://labs.eventnotifications.net/inbox/
+WEBID="https://patrickhochstenbach/demo/labs/id.jsonld"
+FILE="example.jsonld"
+CONTENT_TYPE="application/ld+json"
+
+HEADERS=$(node bin/headers.js ${KEY} ${WEBID} POST ${URL} ${FILE})
+
+LENGTH=$(wc -c < ${FILE})
+
+CMD="curl -s ${HEADERS} -H 'Content-Length: ${LENGTH}' -H 'Content-Type: ${CONTENT_TYPE}' --data-binary '@${FILE}' ${URL}"
+
+eval $CMD
